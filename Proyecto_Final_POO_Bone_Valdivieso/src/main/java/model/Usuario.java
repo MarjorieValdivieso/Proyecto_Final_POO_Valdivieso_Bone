bbdbbe10a86cc98b73d92146c7a4170e0beb37c0
MarjorieValdivieso <marjorievillacis430@gmail.com>
2026-07-05 15:34:08 -0500

    Login con tres roles implementado

--- Proyecto_Final_POO_Bone_Valdivieso/src/main/java/model/Usuario.java
+++ Proyecto_Final_POO_Bone_Valdivieso/src/main/java/model/Usuario.java
@@ -1,4 +1,35 @@
 package model;
 
-public class Usuario {
+public class Usuario implements Autenticable {
+    private int id;
+    private String usuario;
+    private String clave;
+    private String rol;
+
+    public Usuario() {}
+
+    public Usuario(int id, String usuario, String clave, String rol) {
+        this.id = id;
+        this.usuario = usuario;
+        this.clave = clave;
+        this.rol = rol;
+    }
+
+    // getters y setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getUsuario() { return usuario; }
+    public void setUsuario(String usuario) { this.usuario = usuario; }
+
+    public String getClave() { return clave; }
+    public void setClave(String clave) { this.clave = clave; }
+
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
+
+    @Override
+    public boolean validarCredenciales(String usuario, String clave) {
+        return this.usuario.equals(usuario) && this.clave.equals(clave);
+    }
 }
